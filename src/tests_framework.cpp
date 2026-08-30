0d029e08f9d1e5661b4b0d9f872053aadb9979e8
stgatilov <none@none>
2026-08-30 18:36:02 +0100

    Explicitly stop TBB worker threads so that we can use valgrind.

--- src/tests_framework.cpp
+++ src/tests_framework.cpp
@@ -1,2 +1,13 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
+
+#include <tbb/global_control.h>
+
+int main(int argc, char **argv) {
+    tbb::task_scheduler_handle handle{oneapi::tbb::attach{}};
+    int retcode = doctest::Context(argc, argv).run();
+    // explicitly finalizate TBB worker threads
+    // without this, valgrind detects active TBB allocations
+    tbb::finalize(handle);
+    return retcode;
+}
