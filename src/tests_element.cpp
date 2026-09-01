4c87a5294b333b3d15c8624eeff9dc5a8b9dfc33
stgatilov <none@none>
2026-09-01 21:47:51 +0100

    Fixed missing typename on GCC in tests.

--- src/tests_element.cpp
+++ src/tests_element.cpp
@@ -27,7 +27,8 @@ TEST_CASE_TEMPLATE("Element", Element,
     std::uniform_int_distribution<int64_t> distr;
     std::vector<Element> arr = generateArray<Element>(DEFAULT_ARRAY_SIZE, [&]{
         Element x;
-        x.key = (Element::Key)distr(random);
+        typedef typename Element::Key KeyType;
+        x.key = KeyType(distr(random));
         fillValue(x, random);
         return x;
     });
